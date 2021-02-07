7d665b27e17f68437c0b31e6a19e9659ff5ef397
Erkan Ergen <erkanergen35@gmail.com>
2021-02-07 21:56:04 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public bool Count { get; internal set; }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKey = keys;
+            TValue[] tempValue = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        internal void ShowList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " " + values[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> NameAge = new MyDictionary<string, int>();
+            NameAge.Add("aaaaa", 34);
+            NameAge.Add("bbbbb", 20);
+            NameAge.Add("ccccc", 30);
+            NameAge.Add("ddddd", 40);
+
+
+            Console.WriteLine(NameAge.Count);
+
+            NameAge.ShowList();
+
+
+        }
+    }
+}
